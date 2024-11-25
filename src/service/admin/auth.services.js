8bfc561a4a3819da94cf8e3e7ef6da9929f90c2d
Udhaykrishn <krishnauday320@gmail.com>
2024-11-25 17:30:20 +0530

    created the admin signin and logout functions

---
+++ src/service/admin/auth.services.js
@@ -0,0 +1,46 @@
+import { Admin } from "../../model/admin.model.js";
+import { signToken } from "../../utility/jwt.utility.js";
+
+const getAdminLogin = (req, res) => {
+  return res.render("admin/login", { error: null });
+};
+
+const adminLogin = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.render("admin/login", {
+      error: "Admin Email and Password is required",
+    });
+  }
+
+  const user = await Admin.findOne({ email });
+  console.log(Admin);
+  console.log(user);
+  if (!user) {
+    return res.render("admin/login", {
+      error: "Admin is not found",
+    });
+  }
+
+  const payload = {
+    id: user._id,
+    role: user.role,
+  };
+
+  const token = signToken(payload);
+
+  res.cookie("adminToken", token, {
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000,
+  });
+
+  return res.redirect("/admin/dashboard");
+};
+
+const adminLogout = (req, res) => {
+  res.clearCookie("adminToken");
+  res.redirect("/admin/signin");
+};
+
+export { getAdminLogin, adminLogin, adminLogout };

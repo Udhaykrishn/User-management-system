d28f5d5dcf181180a721103c959cc0dd6cf340ce
Udhaykrishn <krishnauday320@gmail.com>
2024-11-25 16:08:48 +0530

    created the admin restrict middleware function

---
+++ src/middleware/restrictToAdmin.middleware.js
@@ -0,0 +1,32 @@
+import { verifyToken } from "../utility/jwt.utility.js";
+
+const restrictToAdmin = (req, res, next) => {
+  try {
+    const token = req.cookies?.adminToken;
+    const userToken = req.cookies?.authToken;
+
+    if (userToken) {
+      return res.redirect("/user/dashboard")
+    }
+
+    if (token) {
+      const decoded = verifyToken(token);
+
+      if (decoded.role === "admin") {
+        return next();
+      } else {
+        return res.status(403).json({
+          error: "Access denied",
+          message: "User does not have admin access",
+        });
+      }
+    }
+    console.log("This is login")
+    return res.redirect("/admin/signin");
+  } catch (error) {
+    console.error("Error verifying token", error.message);
+    return res.status(403).json({ error: "Access denied" });
+  }
+};
+
+export { restrictToAdmin };

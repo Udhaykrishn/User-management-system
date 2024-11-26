240c2bc1739d70f41e966f9d6d40fa14c5d67a6c
Udhaykrishn <krishnauday320@gmail.com>
2024-11-26 15:07:23 +0530

    created main file and config very routes and created the server

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import { config } from "dotenv";
+import nocache from "nocache";
+import srcPath from "./utility/path.utility.js";
+import path from "path";
+import createMorganStreamFile from "#service/morgan/morgan.service.js";
+import connectDB from "#config/db.config.js";
+import Users from "#controller/user/login.controller.js";
+import Admin from "#controller/admin/admin.controller.js";
+import { authMiddleware } from "#middleware/jwt.middleware.js";
+import { userLogin, userSignUp } from "#service/user/login.service.js";
+
+config();
+
+const app = express();
+
+app.use(nocache());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(srcPath, "view"));
+
+const init = async () => {
+  try {
+    const morganStream = await createMorganStreamFile();
+    app.use(morganStream);
+
+    await connectDB();
+    app.use("/api/user", authMiddleware, Users);
+    app.use("/api/admin", Admin);
+    app.use("/api/signup", userSignUp);
+    app.use("/api/login", userLogin);
+
+    const PORT = process.env.PORT;
+
+    app.listen(PORT, () => console.log("server is running on ", PORT));
+  } catch (error) {
+    console.error("Error during application init", error.message);
+  }
+};
+
+init();

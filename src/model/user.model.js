70a0085609111685ad8a50b1ebf79c9aa03b961f
Udhaykrishn <krishnauday320@gmail.com>
2024-11-27 10:27:48 +0530

    password hasing before saving in user model

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -1,8 +1,22 @@
 import { Schema, model } from "mongoose";
+import { hash } from "argon2";
 
 const userSchema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, minLength: 4, maxLength: 10 },
+});
+
+userSchema.pre("save", async (next) => {
+  if (!this.isModified("passwod")) {
+    return next();
+  }
+
+  try {
+    this.password = await hash(this.password);
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 const User = model("User", userSchema);

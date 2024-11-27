78444dfa1d661bde02c9fccf2d9f7785cbe35925
Udhaykrishn <krishnauday320@gmail.com>
2024-11-27 10:24:31 +0530

    added the correct import user services

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import { getLogin, postLogin, userLogout } from "../services/login.service.js";
-import { getSignUp, postSignUp } from "../services/signup.service.js";
-import { AuthCheck } from "../middleware/authchecker.middleware.js";
-import { isAuthenticated } from "../middleware/jwt.middleware.js";
+import {
+  postSignUp,
+  getLogin,
+  postLogin,
+  userLogout,
+  getSignUp,
+} from "#service/user/auth.service.js";
+import { AuthCheck } from "#middleware/authchecker.middleware.js";
+import { isAuthenticated } from "#middleware/jwt.middleware.js";
 
 const router = Router();
 

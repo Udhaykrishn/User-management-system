d9fea5a0cdad7851522cfc964af5d979f83f90c1
Udhaykrishn <krishnauday320@gmail.com>
2024-11-28 17:32:35 +0530

    added the admin check middleware

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -5,7 +5,7 @@ import {
   adminLogout,
 } from "#service/admin/auth.services.js";
 import {
-  CreateOneUser,
+  createOneUser,
   deleteUser,
   editUser,
   getEditUser,
@@ -13,22 +13,25 @@ import {
 } from "#service/admin/curd.services.js";
 import { restrictToAdmin } from "#middleware/restrictToAdmin.middleware.js";
 import { GetAllUsers } from "#repositorys/user/user.repository.js";
+import { adminCheck } from "#middleware/admin.middleware.js";
 
 const router = Router();
 
 router
-  .get("/signin", restrictToAdmin, getAdminLogin)
+  .get("/signin", adminCheck, getAdminLogin)
   .post("/signin", adminLogin)
   .post("/signout", adminLogout);
 
 router.get("/dashboard", restrictToAdmin, async (req, res) => {
   const users = await GetAllUsers();
 
+  console.log(users);
+
   return res.render("admin/panel", { users: users });
 });
 
 router.get("/dashboard/createUser", restrictToAdmin, getCreateUser);
-router.post("/dashboard/createUser", CreateOneUser);
+router.post("/dashboard/createUser", createOneUser);
 router.get("/dashboard/edit/:id", restrictToAdmin, getEditUser);
 router.post("/dashboard/edit/:id", editUser);
 router.delete("/dashboard/delete/:id", deleteUser);

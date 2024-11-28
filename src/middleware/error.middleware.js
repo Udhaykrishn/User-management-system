26cf6bb35ece161bfa483ec5caf0da7cc4d3c15c
Udhaykrishn <krishnauday320@gmail.com>
2024-11-28 10:40:42 +0530

    remove console.log

--- src/middleware/error.middleware.js
+++ src/middleware/error.middleware.js
@@ -1,7 +1,5 @@
 const errorHanlder = (err, req, res, next) => {
-  console.error(err.stack);
-
-  res.status(err.status || 500).json({
+  return res.status(err.status || 500).json({
     success: false,
     message: err.message || "Interval server error",
   });

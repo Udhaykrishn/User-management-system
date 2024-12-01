cc1486bd3da62548603d7b2b263e7a46ac5da3f1
Udhaykrishn <krishnauday320@gmail.com>
2024-12-01 21:52:26 +0530

    added the maximum request and times in request limit

--- src/config/rate-limit.config.js
+++ src/config/rate-limit.config.js
@@ -1,8 +1,8 @@
 import rateLimit from "express-rate-limit";
 
 export const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000,
-  max: 10,
+  windowMs: 15 * 60 * 100,
+  max: 100,
   standardHeaders: true,
   legacyHeaders: false,
   message: "Too many requests from this IP",
